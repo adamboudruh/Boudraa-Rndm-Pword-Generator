798d5b1e7d040181dc71b23d9923713a64147aba
Adam Boudraa <adamboudraa@gmail.com>
2023-11-14 11:42:43 -0600

    Added comments, fixed typos, pWord not generated on load anymore

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 /*  */// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// don't know how to do an event listener yet so I'm just calling the function here
-writePassword();
-
 function generatePassword() {
   var pLength = prompt("How long would you like the password to be? Please enter a number between 8 and 128: ");
-    if (pLength < 8 || pLength > 128) {
+    if (pLength < 8 || pLength > 128) { //ensures that a valid length is used
       alert("Please enter a length between 8 and 128!");
       return "Try again ¯\\_(ツ)_/¯";
     }
   console.log("Length selected: " + pLength);
   var inclLower = confirm("Would you like to include lowercase characters?");
-  console.log("Will password nclude lower? " + inclLower);
+  console.log("Will password include lower? " + inclLower);
   var inclUpper = confirm("Would you like to include uppercase characters?");
-  console.log("Will password nclude upper? " + inclUpper);
+  console.log("Will password include upper? " + inclUpper);
   var inclNum = confirm("Would you like to include numeric characters?");
-  console.log("Will password nclude nums? " + inclNum);
+  console.log("Will password include nums? " + inclNum);
   var inclSpec = confirm("Would you like to include special characters?");
-  console.log("Will password nclude special chars? " + inclSpec);
+  console.log("Will password include special chars? " + inclSpec); //these booleans are used to determine what characters are added to the pool
   var strLow = "abcdefghijklmnopqrstuvwxyz";
   var strUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var strNum = "1234567890";
   var strSpec = "!@#$%&*()+-?.";
-  var strFinal = "";
+  var strFinal = ""; //this is the pool that characters are pulled from the create the password
     if (inclLower) strFinal += strLow;
     if (inclUpper) strFinal += strUp;
     if (inclNum) strFinal += strNum;
@@ -35,9 +32,9 @@ function generatePassword() {
       return "Try again ¯\\_(ツ)_/¯";
     }
     else {
-      for (var i = 0; i <= pLength; i++) {
+      for (var i = 0; i < pLength; i++) { //this loop pulls and appends a random character as many times as is dictated by the pLength
         pWord += strFinal.charAt(Math.floor(Math.random() * (strFinal.length + 1)))
-      }
+      } //Math.floor rounds the number down to ensure it's an integer
     }
     return pWord;
 }
